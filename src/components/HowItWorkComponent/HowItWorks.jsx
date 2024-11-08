926120bb64aa3d7c87eab02fe529b18f9aa2d1f7
Sania Singh <saniasingh4in@gmail.com>
2024-11-08 07:54:24 +0530

    updated HowItWorks.jsx

--- src/components/HowItWorkComponent/HowItWorks.jsx
+++ src/components/HowItWorkComponent/HowItWorks.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
-
 import { HowItWorkcard } from "./HowItWorkcard";
+import howItWorkData from "./howItWorkData.json";
 
 export const HowItWorks = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // console.log(howItWorkData.steps)
+    setData(howItWorkData.steps);
+  }, []);
   return (
     <div>
       <section style={{ justifyContent: "center", margin: "60px 100px" }}>
@@ -32,10 +38,16 @@ export const HowItWorks = () => {
             display: "flex",
           }}
         >
-          <HowItWorkcard />
-          <HowItWorkcard />
-          <HowItWorkcard />
-          <HowItWorkcard />
+          {data.map((step, index) => (
+            <HowItWorkcard
+              key={index}
+              heading={step.heading}
+              icon={step.icon}
+              alt={step.alt}
+              title={step.title}
+              description={step.description}
+            />
+          ))}
         </div>
       </section>
     </div>
